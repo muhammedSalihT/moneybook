259d7877746954b98005d92156788b06bde4588c
Muhammed Salih T <mhdsalih9656@gmail.com>
2022-07-01 14:37:31 +0530

    screen added

--- lib/Screens/Catogeries/income_catogeri_list.dart
+++ lib/Screens/Catogeries/income_catogeri_list.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-class ScreenTrans extends StatelessWidget {
-  const ScreenTrans({Key? key}) : super(key: key);
+class IncomeCatogeryList extends StatelessWidget {
+  const IncomeCatogeryList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  ListView.separated(itemBuilder:(ctx, index){
+     return  ListView.separated(itemBuilder:(ctx, index){
       return const Card(
         child: ListTile(leading: Text("12 DEC"),
         title: Text("1000"),
         subtitle: Text("Travel"),
+        trailing: Icon(Icons.delete)
         ),
       );
     },
@@ -17,6 +18,5 @@ class ScreenTrans extends StatelessWidget {
       return const SizedBox(height: 10,);
      },
       itemCount: 10);
-    
   }
 }
